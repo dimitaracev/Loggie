e40e62375de690c3f136aa63332c8f04d8251ef6
dimitaracev <dimitaracev@protonmail.com>
2020-05-06 18:57:52 +0200

    Renamed, added cleanup macro to cleanup file descriptors

--- src/loggie.h
+++ src/loggie.h
@@ -33,12 +33,22 @@ static int initialized = 0;
         }                                                     \
     }
 
-#define Connect(logger)                                                    \
-    {                                                                      \
-        if (!initialized)                                                  \
-            Init();                                                        \
-        Logger.fds[Logger.files] = logger;                                 \
-        Logger.fds = realloc(Logger.fds, sizeof(FILE *) * ++Logger.files); \
+#define Connect(logger)                                        \
+    {                                                          \
+        if (!initialized)                                      \
+            Init();                                            \
+        Logger.fds[Logger.files] = logger;                     \
+        Logger.fds = realloc(Logger.fds,                       \
+                             sizeof(FILE *) * ++Logger.files); \
+    }
+
+#define Clean()                                \
+    {                                          \
+        for (int i = 0; i < Logger.files; i++) \
+        {                                      \
+            free(Logger.fds[i]);               \
+        }                                      \
+        free(Logger.fds);                      \
     }
 
 #define Log(format, ...)                   \
@@ -61,16 +71,17 @@ static int initialized = 0;
         PRINT("ERROR", format, __VA_ARGS__); \
     }
 
-#define DOMAGIC(OUT, Level, format, ...)                                                                        \
-    {                                                                                                           \
-        time_t t = time(NULL);                                                                                  \
-        struct tm *timeinfo = localtime(&t);                                                                    \
-        fprintf(OUT, "[%02d:%02d:%02d  %02d/%02d/%04d]", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, \
-                timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900);                             \
-        fprintf(OUT, " [FILE: %s | LINE: %d] ", __FILE__, __LINE__);                                            \
-        fprintf(OUT, "%s  :  ", Level);                                                                       \
-        fprintf(OUT, format, __VA_ARGS__);                                                                      \
-        fprintf(OUT, "\n");                                                                                     \
+#define DOMAGIC(OUT, Level, format, ...)                                    \
+    {                                                                       \
+        time_t t = time(NULL);                                              \
+        struct tm *timeinfo = localtime(&t);                                \
+        fprintf(OUT, "[%02d:%02d:%02d  %02d/%02d/%04d]", timeinfo->tm_hour, \
+                timeinfo->tm_min, timeinfo->tm_sec, timeinfo->tm_mday,      \
+                timeinfo->tm_mon + 1, timeinfo->tm_year + 1900);            \
+        fprintf(OUT, " [FILE: %s | LINE: %d] ", __FILE__, __LINE__);        \
+        fprintf(OUT, "%s  :  ", Level);                                     \
+        fprintf(OUT, format, __VA_ARGS__);                                  \
+        fprintf(OUT, "\n");                                                 \
     }
 
 #define PRINT(Level, format, ...)                               \
